b7b869119b2077b2824327196dfdde9907be55b4
iDr4w <josep.david.montoya@gmail.com>
2021-02-05 21:55:45 +0100

    prueba

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,6 @@
 var nav = document.querySelector("#nav");
 var menu = document.querySelector("#burger_menu");
 var links = document.querySelector(".links");
-var html = document.querySelector("html");
 var results = document.querySelectorAll(".game-result");
 var dropdown = document.querySelectorAll(".dropdown");
 let navTop = nav.offsetTop;
@@ -24,16 +23,8 @@ menu.addEventListener("click", () => {
   }
   menu.classList.toggle('open');
   links.classList.toggle('open');
-  html.classList.toggle('open');
 });
 
-window.addEventListener("scroll",() => {
-  if (window.scrollY >= navTop) {    
-    nav.classList.add('fixed');
-  } else {
-    nav.classList.remove('fixed');    
-  }
-});
 
 var xhReq = new XMLHttpRequest();
 xhReq.open("GET", "https://spreadsheets.google.com/feeds/cells/1gVcxUeGsA0JEX6kLBPKC5S2NtY2uwhLvky9T-vEr2eI/1/public/values?alt=json", false);  
