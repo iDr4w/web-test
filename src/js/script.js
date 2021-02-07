d86d45523e08a6ac30970cdb3d5805a1f3931574
iDr4w <josep.david.montoya@gmail.com>
2021-02-07 23:23:38 +0100

    V.1

--- src/js/script.js
+++ src/js/script.js
@@ -50,6 +50,8 @@ xhReq.send(null);
 var jsonObject = JSON.parse(xhReq.responseText);
 entry = jsonObject.feed.entry;
 
+document.getElementById("jornada").innerHTML = "JORNADA " + jsonObject.feed.title.$t;
+
 for (let i = 0; i < entry.length - 3; i++) {
     results[i].innerHTML = entry[i + 3].content.$t;
 }
